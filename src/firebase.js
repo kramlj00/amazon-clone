564398724b09f5a3476ecf14f53e058c822f6934
kramlj00 <73222835+kramlj00@users.noreply.github.com>
2021-05-29 09:33:41 +0200

    Adding firebase.js file

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "AIzaSyCfHhO3_hsyrPJbl5v-aeENSgPx51wCHKU",
+  authDomain: "clone-5de79.firebaseapp.com",
+  projectId: "clone-5de79",
+  storageBucket: "clone-5de79.appspot.com",
+  messagingSenderId: "1006977385292",
+  appId: "1:1006977385292:web:770af9d99779d32764c5e9",
+});
+
+const db = firebase.firestore();
+
+export { db };
