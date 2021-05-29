39d30f458f78cd5f9919afe8be968a9f1ec0f53d
kramlj00 <73222835+kramlj00@users.noreply.github.com>
2021-05-29 16:06:29 +0200

    Styling the Login page

--- src/Login.js
+++ src/Login.js
@@ -2,9 +2,50 @@ import React from "react";
 import styled from "styled-components";
 
 function Login() {
-  return <Container>Login</Container>;
+  return (
+    <Container>
+      <Content>
+        <AmazonLogo src="http://media.corporate-ir.net/media_files/IROL/17/176060/Oct18/Amazon%20logo.PNG" />
+        <h1>Sign into Amazon</h1>
+        <LoginButton>Sign in with Google</LoginButton>
+      </Content>
+    </Container>
+  );
 }
 
 export default Login;
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #f8f8f8;
+  display: grid;
+  place-items: center;
+`;
+
+const Content = styled.div`
+  padding: 100px;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 0 1px 3px gray;
+  text-align: center;
+`;
+
+const AmazonLogo = styled.img`
+  height: 100px;
+  margin-bottom: 35px;
+`;
+
+const LoginButton = styled.button`
+  margin-top: 50px;
+  background-color: #f0c14b;
+  height: 40px;
+  border: 2px solid #a88734;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 16px;
+  padding: 4px 8px;
+  :hover {
+    background: #ddb347;
+  }
+`;
